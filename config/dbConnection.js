8ad6a4e9b0feafe29c4f660a7f0c88d1d9f26a17
Birender Prajapati <98900878+fitindiahitindia@users.noreply.github.com>
2025-01-09 22:41:36 +0530

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -4,7 +4,7 @@ mongoose.set("strictQuery",false);
 
 const dbConnect = async() =>{
     try{
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL,{useUnifiedTopology: true,useNewUrlParser: true,});
         console.log("DB Connected Successfully")
     }
     catch(error){
