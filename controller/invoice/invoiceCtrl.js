6705c64b55b1c9fef47e1d6dca572591878a1878
Birender Prajapati <98900878+fitindiahitindia@users.noreply.github.com>
2025-01-16 21:52:39 +0530

    Update invoiceCtrl.js

--- controller/invoice/invoiceCtrl.js
+++ controller/invoice/invoiceCtrl.js
@@ -38,7 +38,9 @@ exports.downloadInvoice = expressAsyncHandler(async (req, res) => {
       return;
     }
     console.log(`${req.protocol}://${req.get("host")}` + "/api/v1/invoice/viewInvoice/"+req.params.id)
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch(
+      { headless: true, args: ['--no-sandbox'] }
+    );
     const page = await browser.newPage();
     await page.goto (
       `${req.protocol}://${req.get("host")}` + "/api/v1/invoice/viewInvoice/"+req.params.id,
